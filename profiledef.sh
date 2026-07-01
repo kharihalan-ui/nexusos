322de919598e66b731ad10e5192e50bf1128013c
kharihalan-ui <kharihalan-ui@users.noreply.github.com>
2026-07-01 10:43:41 +0200

    Fix: remove file_permissions for non-existent files (shadow, gshadow, gnupg)

--- profiledef.sh
+++ profiledef.sh
@@ -16,8 +16,5 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '3' '-b' '256K')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/etc/gshadow"]="0:0:400"
   ["/root/customize_os.sh"]="0:0:755"
-  ["/root/.gnupg"]="0:0:700"
 )
